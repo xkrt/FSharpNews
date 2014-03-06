d310909b5b40b269d8ce35256e6d10423df20142
pmartynov <pavel.martynov@kaspersky.com>
2014-03-06 08:05:47 +0400

    fssnip: fix bug with published yesterday

--- FSharpNews.Data/Fssnip.fs
+++ FSharpNews.Data/Fssnip.fs
@@ -17,6 +17,7 @@ let private toDateTime (publishedAgo: string) =
                | Regex.Interpreted.Match "(\\d+) secs ago" m -> m |> double |> TimeSpan.FromSeconds
                | Regex.Interpreted.Match "(\\d+) mins ago" m -> m |> double |> TimeSpan.FromMinutes
                | Regex.Interpreted.Match "(\\d+) hours ago" m -> m |> double |> TimeSpan.FromHours
+               | Regex.Interpreted.Match "yesterday" _ -> TimeSpan.FromDays(1.)
                | Regex.Interpreted.Match "(\\d+) days ago" m -> m |> double |> TimeSpan.FromDays
                | Regex.Interpreted.Match "(\\d+) months ago" m -> m |> double |> (*) 30. |> TimeSpan.FromDays
                | Regex.Interpreted.Match "(\\d+) years ago" m -> m |> double |> (*) 365. |> TimeSpan.FromDays
