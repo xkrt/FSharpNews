72dadb3efa65444330bf108e20a976d5b2db9301
pmartynov <pavel.martynov@kaspersky.com>
2014-03-13 10:11:15 +0400

    Better loader appearance

--- FSharpNews.Web.Frontend/Scripts/knockout.extensions.js
+++ FSharpNews.Web.Frontend/Scripts/knockout.extensions.js
@@ -5,12 +5,12 @@ ko.bindingHandlers.scroll = {
         var loadFunc = valueAccessor();
         var off = function () { $(window).off("scroll.ko.scrollHandler"); };
 
-        element.style.display = "none";
+        element.style.visibility = "hidden";
         $(window).on("scroll.ko.scrollHandler", function () {
             if ($(document).height() - offset <= $(window).height() + $(window).scrollTop()) {
-                element.style.display = "";
+                element.style.visibility = "visible";
                 loadFunc().always(function(data) {
-                    element.style.display = "none";
+                    element.style.visibility = "hidden";
                     if (data.length === 0)
                         off();
                 });
